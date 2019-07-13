3dcc781a04b65036543be3d1251f53e6b1422d86
Maiker Gonzales <maiker.gonzales@vallegrande.edu.pe>
2019-07-13 13:48:27 -0500

    base de datos subida

---
+++ Tables/MAESTRA.PERSONA.sql
@@ -0,0 +1,16 @@
+CREATE TABLE [MAESTRA].[PERSONA]
+(
+[IDPER] [int] NOT NULL IDENTITY(1, 1),
+[NOMPER] [varchar] (150) COLLATE Modern_Spanish_CI_AS NULL,
+[APEPER] [varchar] (150) COLLATE Modern_Spanish_CI_AS NULL,
+[DNIPER] [char] (8) COLLATE Modern_Spanish_CI_AS NULL,
+[SEXPER] [char] (1) COLLATE Modern_Spanish_CI_AS NULL,
+[CELPER] [char] (9) COLLATE Modern_Spanish_CI_AS NULL,
+[TIPPER] [char] (1) COLLATE Modern_Spanish_CI_AS NULL,
+[USUPER] [varchar] (50) COLLATE Modern_Spanish_CI_AS NULL,
+[PASSPER] [varchar] (50) COLLATE Modern_Spanish_CI_AS NULL,
+[ESTPER] [char] (1) COLLATE Modern_Spanish_CI_AS NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [MAESTRA].[PERSONA] ADD CONSTRAINT [PK__PERSONA__98FE8B7E369C7892] PRIMARY KEY CLUSTERED  ([IDPER]) ON [PRIMARY]
+GO
