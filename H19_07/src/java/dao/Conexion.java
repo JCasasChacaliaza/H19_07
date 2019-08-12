8487bc23db4d2b816868acc5b072848a298f3c85
JCasasChacaliaza <james.casas@vallegrande.edu.pe>
2019-08-12 00:41:54 -0500

    login

--- H19_07/src/java/dao/Conexion.java
+++ H19_07/src/java/dao/Conexion.java
@@ -12,7 +12,7 @@ public class Conexion {
         try {
             if (conectar == null) {
                 Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-                conectar = DriverManager.getConnection("jdbc:sqlserver://localhost; database=H19_07_BD", "usuarioSQL", "ads_thony");
+                conectar = DriverManager.getConnection("jdbc:sqlserver://192.168.8.122; database=demoII", "admin", "admin");
             }
         } catch (ClassNotFoundException | SQLException e) {
             System.out.println("Error " + e);
